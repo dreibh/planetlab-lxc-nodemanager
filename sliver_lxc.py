0214164541351badc2c248962ee2cbd756d8e8a7
Sapan Bhatia <gwsapan@gmail.com>
2012-09-05 09:27:37 -0400

    Switch back to vsh, now provided by lxc tools

--- sliver_lxc.py
+++ sliver_lxc.py
@@ -18,7 +18,7 @@ from sliver_libvirt import Sliver_Libvirt
 class Sliver_LXC(Sliver_Libvirt, Initscript):
     """This class wraps LXC commands"""
 
-    SHELL = '/bin/sshsh'
+    SHELL = '/bin/vsh'
     TYPE = 'sliver.LXC'
     # Need to add a tag at myplc to actually use this account
     # type = 'sliver.LXC'
