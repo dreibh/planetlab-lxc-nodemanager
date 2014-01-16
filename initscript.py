716cbd2dbcf887e7eea05053a04cae3ddef7073a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-01-16 10:11:51 +0100

    fix misleading comments that improperly refer to vsliver_vs on an lxc node

--- initscript.py
+++ initscript.py
@@ -31,14 +31,14 @@ class Initscript:
         # install in sliver
         code=file(vinit_source).read()
         if tools.replace_file_with_string(vinit_script,code,chmod=0755):
-            logger.log("vsliver_vs: %s: installed generic vinit rc script"%self.name)
+            logger.log("Initscript: %s: installed generic vinit rc script"%self.name)
         # create symlink for runlevel 3
         if not os.path.islink(rc3_link):
             try:
-                logger.log("vsliver_vs: %s: creating runlevel3 symlink %s"%(self.name,rc3_link))
+                logger.log("Initscript: %s: creating runlevel3 symlink %s"%(self.name,rc3_link))
                 os.symlink(rc3_target,rc3_link)
             except:
-                logger.log_exc("vsliver_vs: %s: failed to create runlevel3 symlink %s"%rc3_link)
+                logger.log_exc("Initscript: %s: failed to create runlevel3 symlink %s"%rc3_link)
 
     # install or remove the slice inistscript, as instructed by the initscript tag
     def refresh_slice_vinit(self):
@@ -46,7 +46,7 @@ class Initscript:
         sliver_initscript="/vservers/%s/etc/rc.d/init.d/vinit.slice"%self.name
         if tools.replace_file_with_string(sliver_initscript,code,remove_if_empty=True,chmod=0755):
             if code:
-                logger.log("vsliver_vs: %s: Installed new initscript in %s"%(self.name,sliver_initscript))
+                logger.log("Initscript: %s: Installed new initscript in %s"%(self.name,sliver_initscript))
                 if self.is_running():
                     # Only need to rerun the initscript if the vserver is
                     # already running. If the vserver isn't running, then the
@@ -54,5 +54,5 @@ class Initscript:
                     # /etc/rc.d/vinit when the vserver is started.
                     self.rerun_slice_vinit()
             else:
-                logger.log("vsliver_vs: %s: Removed obsolete initscript %s"%(self.name,sliver_initscript))
+                logger.log("Initscript: %s: Removed obsolete initscript %s"%(self.name,sliver_initscript))
 
